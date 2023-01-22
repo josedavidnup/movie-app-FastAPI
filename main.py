8f221411afa14f7a6b9a521282395b001484325d
josedavidnup <josedavidnup@gmail.com>
2023-01-21 21:27:17 -0400

    Post Method

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
@@ -12,11 +12,11 @@ movies = [
         "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
         "year": "2009",
         "rating": 7.8,
-        "category": "Acción",
+        "category": "Fiction",
     },
     {
         "id": 2,
-        "title": "Vengadores",
+        "title": "Avengers",
         "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
         "year": "2009",
         "rating": 7.8,
@@ -29,6 +29,22 @@ movies = [
         "year": "2009",
         "rating": 7.8,
         "category": "Acción",
+    },
+    {
+        "id": 4,
+        "title": "Super Man",
+        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        "year": "2009",
+        "rating": 7.8,
+        "category": "Comic",
+    },
+    {
+        "id": 5,
+        "title": "Tictanic",
+        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        "year": "2009",
+        "rating": 7.8,
+        "category": "Drama",
     },
 ]
 
@@ -43,7 +59,7 @@ def get_movies():
     return movies
 
 
-@app.get("/movies/{id}")
+@app.get("/movies/{id}", tags=["movies"])
 def get_movie(id: int):
     movie = list(filter(lambda x: x["id"] == id, movies))
     # for item in movies:
@@ -54,3 +70,44 @@ def get_movie(id: int):
     # return movie if len(movie) > 0 else "No existe"
     return "No se encontró la película" if not movie else movie[0]
     # return movie[0] or "No hay nada que ver"
+
+
+@app.get("/movies/", tags=["movies"])
+# def get_movies_by_category(category: str, year: int):
+def get_movies_by_category(category: str):
+    # return [ item for item in movies if item['category'] == category ]
+    movie = list(filter(lambda x: x["category"].lower() == category.lower(), movies))
+    return movie
+
+
+@app.post("/movies", tags=["movies"])
+def create_movie(
+    id: int = Body(),
+    title: str = Body(),
+    overview: str = Body(),
+    year: int = Body(),
+    rating: float = Body(),
+    category: str = Body(),
+):
+    movies.append(
+        {
+            "id": id,
+            "title": title,
+            "overview": overview,
+            "year": year,
+            "rating": rating,
+            "category": category,
+        }
+    )
+    return movies
+    # De esta forma no se valida los datos -------->
+    # from fastapi import Request
+    # movie = await request.json()
+    # movies.append(movie)
+    # return movie
+    # Error TypeError: Body() missing 1 required positional argument: 'default'
+    # def create_movie(id: int = Body(default=1),
+    #     title: str = Body(default="text"),
+    #     year:int = Body(default=0),
+    #     rating: float = Body(default=0),
+    #     category: str = Body(default="text")):
